22941990eae6aaece6d78e9b268a9d7368649c56
rudyravelindev <rudyravelindev@gmail.com>
2026-05-14 11:11:20 -0700

    256 grid created using js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const container = document.getElementById('container');
+
+// Create 256 divs (16 * 16)
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div');
+  square.classList.add('grid-item');
+  container.appendChild(square);
+}
